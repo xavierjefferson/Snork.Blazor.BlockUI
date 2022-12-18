edd5a23fff975edb4a9a60ca3a94526c8324883f
Xavier Jefferson <xjefferson@amada.com>
2022-12-17 17:28:41 -0800

    Add project files.

---
+++ Snork.BlockUI/ContainerCss.cs
@@ -0,0 +1,19 @@
+﻿namespace Snork.Blazor.BlockUI
+{
+    public class ContainerCss
+    {
+        public string Padding { get; set; } = "0";
+        public string Margin { get; set; } = "0";
+        public string Width { get; set; } = "30%";
+
+        public string Top { get; set; } = "40%";
+
+        public string Left { get; set; } = "35%";
+        public string TextAlign { get; set; } = "center";
+        public string Color { get; set; } = "#000";
+        public string Border { get; set; } = "3px solid #aaa";
+
+        public string BackgroundColor { get; set; } = "#fff";
+        public string Cursor { get; set; } = "wait";
+    }
+}
---
+++ Snork.BlockUI/OverlayCss.cs
@@ -0,0 +1,9 @@
+﻿namespace Snork.Blazor.BlockUI
+{
+    public class OverlayCss
+    {
+        public string BackgroundColor { get; set; } = "#000";
+        public float Opacity { get; set; } = 0.6f;
+        public string Cursor { get; set; } = "wait";
+    }
+}
